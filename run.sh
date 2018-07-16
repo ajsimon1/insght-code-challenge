768c8c4827b752320bb998350504cb9e1183b709
beachboy732 <adamjsimon1@yahoo.com>
2018-07-16 13:44:51 -0400

    updated run.sh file

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 #
 # shell script to run app, args are [1] input file and [2] output file
 # scripts should be ran from top level directory
-python3 /src/insight_drug_cost_sort.py /input/insight_input_100.txt /output/insight_drug_cost_sort.txt
+python3 src/insight_drug_cost_sort.py input/insight_input_100.txt output/insight_drug_cost_sort.txt
