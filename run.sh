65fae609d36237ba1e6961e4dfc705fcf080cd0e
beachboy732 <adamjsimon1@yahoo.com>
2018-07-16 14:17:39 -0400

    edited run file to change input/output filenames

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # shell script to run app, args are [1] input file and [2] output file
 # scripts should be ran from top level directory
-python src/insight_drug_cost_sort.py input/insight_input_100.txt output/insight_drug_cost_sort.txt
+python src/insight_drug_cost_sort.py input/itcont.txt output/top_cost_drug.txt
